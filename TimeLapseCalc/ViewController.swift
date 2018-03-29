b96026b17cb9528ed288059268ac31fe4f67bf33
Alex <aliaksandr.karzhenka@gmail.com>
2018-03-29 16:33:55 +0300

    Initial Commit

---
+++ TimeLapseCalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimeLapseCalc
+//
+//  Created by Alex on 3/29/18.
+//  Copyright © 2018 Aliaksandr Karzhenka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
